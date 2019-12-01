b23c122574355cce6f3cd6fe2c08ed93c5be7591
Shubhcr7 <shubhamsharmacr7@gmail.com>
2019-12-02 00:54:01 +0530

    Register done

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
+import swal from 'sweetalert';
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http:HttpClient) { }
 
   ngOnInit() {
   }
+  submit(f) {
+    console.log(f.value);
 
+    swal({
+      title: "Are you sure to register ?",
+      text: "Please ensure to enter valid account details",
+      icon: "warning",
+      dangerMode: true,
+      buttons: true
+    })
+      .then(willPublish => {
+        if (willPublish) {
+          swal({
+            title: 'Now registering...',
+            buttons: false
+          });
+          this.http.post(environment.url + 'createcampaign', f.value).subscribe(res => {
+            swal("Registered", "You have been successfully registered", "Success");
+          })
+
+        }
+      });
+
+
+  }
 }
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,15 +1,20 @@
+import { Router } from '@angular/router';
+import { CheckLoginService } from './../check-login.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
 export class UserProfileComponent implements OnInit {
-
-  constructor() { }
+  user_name="Shubham Sharma";
+  proj_number=10;
+  constructor(public checkLogin:CheckLoginService,public router:Router) { }
 
   ngOnInit() {
+   if(!this.checkLogin.isValid()){
+      this.router.navigate(['\login']);
+   }
   }
 
 }
